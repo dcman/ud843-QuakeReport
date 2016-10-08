73b1aae6e92f121fd02c6814a51255b75b152fde
dcman <justin.johnson.90@my.csun.edu>
2016-10-07 21:24:50 -0700

    Networking in Quake Report App 15/25 done pulls data from the web

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,36 +15,148 @@
  */
 package com.example.android.quakereport;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
-    public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    public static final String TAG = EarthquakeActivity.class.getName();
+    final String url = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+    private String json;
+    private Context mContext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+        // Create a new AsyncNetworkTask
+        AsyncNetworkTask task = new AsyncNetworkTask();
+        task.execute(url);
 
-        // Find a reference to the {@link ListView} in the layout
-        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+    }
+    private class AsyncNetworkTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            if (params.length < 1 || params[0] == null) {
+                return null;
+            }
 
-        // Create a new {@link ArrayAdapter} of earthquakes
-        ArrayAdapter<Earthquake> adapter = new EarthquakeAdapter(
-                this, R.layout.earth_quake_item, earthquakes);
+            return fetchEarthquakeData(params[0]);
+        }
 
-        // Set the adapter on the {@link ListView}
-        // so the list can be populated in the user interface
-        if (earthquakeListView != null) {
-            earthquakeListView.setAdapter(adapter);
+        @Override
+        protected void onPostExecute(String json) {
+            super.onPostExecute(json);
+
+            // Create list of earthquake locations.
+            ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes(json);
+
+            // Find a reference to the {@link ListView} in the layout
+            ListView earthquakeListView = (ListView) findViewById(R.id.list);
+
+            // Create a new {@link ArrayAdapter} of earthquakes
+            ArrayAdapter<Earthquake> adapter = new EarthquakeAdapter(
+                    mContext, R.layout.earth_quake_item, earthquakes);
+
+            // Set the adapter on the {@link ListView}
+            // so the list can be populated in the user interface
+            if (earthquakeListView != null) {
+                earthquakeListView.setAdapter(adapter);
+            }
+        }
+
+        private String fetchEarthquakeData(String requestUrl) {
+            String jsonReturn = null;
+
+            // Create URL object
+            URL url = createUrl(requestUrl);
+
+            // Perform HTTP request to the URL and receive a JSON response back
+            try {
+                jsonReturn = makeHttpRequest(url);
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing input stream", e);
+            }
+
+            return jsonReturn;
+        }
+
+        private URL createUrl(String requestUrl) {
+            URL url = null;
+            try{
+                url = new URL(requestUrl);
+            } catch (MalformedURLException e) {
+                Log.e(TAG, "Error with creating URL ", e);
+            }
+            return url;
+        }
+
+        private String makeHttpRequest(URL url) throws IOException {
+            String jsonResponse = "";
+
+            // If the URL is null, then return early.
+            if (url == null) {
+                Log.e(TAG, "makeHttpRequest: null URL" );
+                return jsonResponse;
+            }
+
+            HttpURLConnection urlConnection = null;
+            InputStream inputStream = null;
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000 /* milliseconds */);
+                urlConnection.setConnectTimeout(15000 /* milliseconds */);
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // If the request was successful (response code 200),
+                // then read the input stream and parse the response.
+                if (urlConnection.getResponseCode() == 200) {
+                    inputStream = urlConnection.getInputStream();
+                    jsonResponse = readFromStream(inputStream);
+                } else {
+                    Log.e(TAG, "Error response code: " + urlConnection.getResponseCode());
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "Problem retrieving the earthquake JSON results.", e);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            }
+            return jsonResponse;
+        }
+        private String readFromStream(InputStream inputStream) throws IOException {
+            StringBuilder output = new StringBuilder();
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while (line != null) {
+                    output.append(line);
+                    line = reader.readLine();
+                }
+            }
+            return output.toString();
         }
     }
 }
